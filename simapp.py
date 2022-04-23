a8e08e55971c1f1ed8b1a4a262b344c63a87de11
theurikenneth <theurikenneth@gmail.com>
2022-04-23 12:37:53 +0300

    Update simapp.py

--- simapp.py
+++ simapp.py
@@ -22,3 +22,29 @@ while i>0:
             print("Sim Blocked!!")
         else:
              print("You have",i,"attempts left")
+
+
+"""
+IMPROVEMENTS INCLUDE:
+- the user has two pins when setting
+"""
+
+first_pin = int(input("Enter your pin:\n"))
+repeat_pin = int(input("Re-enter the pin:\n"))
+
+if first_pin == repeat_pin:
+    i = 3
+    while i > 0:
+        pin = int(input("Enter your pin:\n"))
+
+        if pin == first_pin:
+            print("Login Successful")
+            break
+        elif pin != first_pin:
+            i -= 1
+            if i == 0:
+                print("Sim Blocked!!")
+            else:
+                print("You have", i, "attempts left")
+else:
+    print("The pin entries do not match")
